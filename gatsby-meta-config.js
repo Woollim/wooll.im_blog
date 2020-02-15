21a42b8c9c2bf034961d115804585444d813eaa5
이병찬 <galaxy000213@gmail.com>
2020-02-15 21:30:53 +0900

    Meta Config 파일 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,29 +1,25 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `Woollim's LOG`,
+  description: `개발자로서 살아가면서 체득한 경험을 공유합니다.`,
+  author: `woollim`,
+  introduction: `좋은 서비스는 보는 사람으로부터 "와우"를 외치게 하고,\n좋은 코드는 보는 사람으로 하여금 "별거 아니였네"라는 자신감을 주어야 한다.`,
+  siteUrl: `https://wooll.im`,
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    github: `Woollim`,
+    facebook: `100004233190518`,
+    linkedin: `병찬-이-726711163`
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    disqusShortName: 'disqus_RfiMXMzhPq', // Your disqus-short-name. check disqus.com.
+    utterances: 'Woollim/wooll.im_blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
-  sponsor: {
-    buyMeACoffeeId: 'jbee',
-  },
   share: {
-    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+    facebookAppId: '100004233190518', // Add facebookAppId for using facebook share feature v3.2
   },
   ga: '', // Add your google analytics tranking ID
 }
